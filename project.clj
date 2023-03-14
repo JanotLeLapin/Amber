bd04eab61b5fdb834e1577aa2cb13b27cb4baf24
Joseph DALY <joseph300905@gmail.com>
2023-03-14 21:36:03 +0000

    Add cljfmt

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL-3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0"}
+  :plugins [[lein-cljfmt "0.9.2"]]
   :dependencies [[org.clojure/clojure "1.10.1"],
                  [org.clojure/data.json "2.4.0"],
                  [org.clj-commons/clj-http-lite "1.0.13"]]
